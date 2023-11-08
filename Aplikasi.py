e55e54dcdce3774956e8bc6b56bd450241f5c3f7
paidjoe007 <121071549+paidjoe007@users.noreply.github.com>
2023-11-08 23:53:07 +0700

    Update Aplikasi.py

--- Aplikasi.py
+++ Aplikasi.py
@@ -6,7 +6,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2
 import streamlit as st
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
+
+# Menampilkan gambar
+image = st.image('icd_image.png', use_column_width=True, caption='Gambar ICD')
 
 df = pd.read_csv('dataset.csv', encoding='latin1')
 df = df.sample(frac=1)
@@ -26,15 +29,14 @@ pipeline = Pipeline([('vect', vectorizer),
 
 model = pipeline.fit(X_train, y_train)
 
-def predict_kode(txt):
-    diagnosis_data = {'predict_kode': [txt]}
-    diagnosis_data_df = pd.DataFrame(diagnosis_data)
-    predict_diagnosis_cat = model.predict(diagnosis_data_df['predict_kode'])[0]
-    return predict_diagnosis_cat
-
 st.title("Automatic ICD Menggunakan Machine Learning")
-diagnosis = st.text_area('Input Diagnosis Penyakit')
-if st.button('Automatic Coding'):
-    st.write("Kode ICD = ", predict_kode(diagnosis))
-    accuracy = model.score(X_test, y_test)
-    st.write("Akurasi: {:.2f}%".format(accuracy * 100))
+
+# Menggunakan st.card untuk tampilan kartu nama
+with st.card:
+    st.subheader("Input Diagnosis Penyakit")
+    diagnosis = st.text_area('Masukkan Diagnosis Penyakit')
+    if st.button('Automatic Coding'):
+        st.subheader("Hasil")
+        st.write("Kode ICD = ", predict_kode(diagnosis))
+        accuracy = model.score(X_test, y_test)
+        st.write("Akurasi: {:.2f}%".format(accuracy * 100))
