2ee83181b63747f63c4eb0d6f9e6ef428c3a531d
paidjoe007 <121071549+paidjoe007@users.noreply.github.com>
2023-11-08 23:41:08 +0700

    Update Aplikasi.py

--- Aplikasi.py
+++ Aplikasi.py
@@ -1,38 +1,36 @@
 import numpy as np
 import pandas as pd
-import re,string
+import re, string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2
 import streamlit as st
+from sklearn.ensemble import RandomForestClassifier  # Ganti impor model
 
-df = pd.read_csv('dataset.csv', encoding = 'latin1')
-df = df.sample(frac = 1)
-
-from sklearn.linear_model import LogisticRegression
-log_regression = LogisticRegression()
+df = pd.read_csv('dataset.csv', encoding='latin1')
+df = df.sample(frac=1)
 
 vectorizer = TfidfVectorizer(stop_words="english")
 
 X = df['Diagnosis']
 Y = df['Label']
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15) #Splitting dataset
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)  # Splitting dataset
 
-# #Creating Pipeline
+# Creating Pipeline
 pipeline = Pipeline([('vect', vectorizer),
-                     ('chi',  SelectKBest(chi2, k=13)),
-                     ('clf', LogisticRegression(random_state=0))])
+                     ('chi', SelectKBest(chi2, k=13)),
+                     ('clf', RandomForestClassifier(random_state=0))]  # Ganti model
+                   )
 
 model = pipeline.fit(X_train, y_train)
 
 def predict_kode(txt):
-    diagnosis_data = {'predict_kode':[txt]}
+    diagnosis_data = {'predict_kode': [txt]}
     diagnosis_data_df = pd.DataFrame(diagnosis_data)
     predict_diagnosis_cat = model.predict(diagnosis_data_df['predict_kode'])[0]
     return predict_diagnosis_cat
-
 
 st.title("Automatic ICD Menggunakan Machine Learning")
 diagnosis = st.text_area('Input Diagnosis Penyakit')
@@ -40,4 +38,3 @@ if st.button('Automatic Coding'):
     st.write("Kode ICD = ", predict_kode(diagnosis))
     accuracy = model.score(X_test, y_test)
     st.write("Akurasi: {:.2f}%".format(accuracy * 100))
-
