bccd38d59d9b33da4ce5d75c89ba11fc7e967d5e
paidjoe007 <121071549+paidjoe007@users.noreply.github.com>
2023-11-08 23:26:42 +0700

    Update Aplikasi.py

--- Aplikasi.py
+++ Aplikasi.py
@@ -26,6 +26,8 @@ pipeline = Pipeline([('vect', vectorizer),
                      ('clf', LogisticRegression(random_state=0))])
 
 model = pipeline.fit(X_train, y_train)
+accuracy = model.score(X_train, y_train)
+
 
 def predict_kode(txt):
     diagnosis_data = {'predict_kode':[txt]}
@@ -37,6 +39,5 @@ def predict_kode(txt):
 st.title("Automatic ICD Menggunakan Machine Learning")
 diagnosis = st.text_area('Input Diagnosis Penyakit')
 if st.button('Automatic Coding'):
-  accuracy = model.score(X_train, y_train)
 st.write("Akurasi Model: {:.2f}%".format(accuracy * 100))
     st.write("Kode ICD = ", predict_kode(diagnosis))
